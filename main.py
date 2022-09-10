aacef48389b43a1d8c09fe25fbed48b64f734f7f
OilRabbit <kyeliaa@connect.ust.hk>
2022-09-10 17:27:44 +0800

    finished

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 from datetime import datetime
 import math
+import decimal
 
+race_time = 0
 counter = 0
 running = False
-batt = 0
+batt = 100
+count_up = False
+one_lap = 0
 
 def S2Time(secs):
     hours = (secs / (60 * 60)) % 24
@@ -15,35 +19,66 @@ def S2Time(secs):
     seconds = math.trunc(seconds)
     return hours, minutes, seconds
 
-def add_time(time_label, t_type):
+def add_time(time_label, onelap_label, t_type, timer):
     global counter
-    if t_type == 'hr':
-        counter += 3600
-    elif t_type == 'min':
-        counter += 60
+    global race_time
+    global one_lap
+    if timer == 'counter':
+        if t_type == 'hr':
+            counter += 3600
+        elif t_type == 'min':
+            counter += 60
+        else:
+            counter += 1
     else:
-        counter += 1
+        if t_type == 'hr':
+            one_lap += 3600
+        elif t_type == 'min':
+            one_lap += 60
+        else:
+            one_lap += 1
     hrs, mins, secs = S2Time(counter)
     display = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(hrs, mins, secs)
     time_label['text'] = display
+    hrs, mins, secs = S2Time(one_lap)
+    onelap_display = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(hrs, mins, secs)
+    onelap_label['text'] = onelap_display
+    race_time = counter
 
-def counter_label(time_label):
+def counter_label(time_label, batt_label):
     def count():
+        global counter
+        global batt
+        global count_up
+        global one_lap
         if running:
-            global counter
-            hrs, mins, secs = S2Time(counter)
-            display = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(hrs, mins, secs)
-            time_label['text']=display   # Or label.config(text=display)
-            time_label.after(1000, count) 
-            counter -= 1
-    # Triggering the start of the counter.
+            if not count_up:
+                hrs, mins, secs = S2Time(counter)
+                display = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(hrs, mins, secs)
+                time_label['text']=display   # Or label.config(text=display)
+                batt = ((counter + one_lap) / (race_time + one_lap)) * 100
+                batt_display = "Batt: %.2f" % (batt)
+                batt_label['text'] = batt_display
+                time_label.after(1000, count)
+                counter -= 1
+                if counter == 0:
+                    count_up = True
+            else:
+                hrs, mins, secs = S2Time(counter)
+                display = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(hrs, mins, secs)
+                time_label['text']=display   # Or label.config(text=display)
+                batt = ((-counter + one_lap) / (race_time + one_lap)) * 100
+                batt_display = "Batt: %.2f" % (batt)
+                batt_label['text'] = batt_display
+                time_label.after(1000, count) 
+                counter += 1
     count()     
 
 # start function of the stopwatch
-def Start(time_label):
+def Start(time_label, batt_label):
     global running
     running=True
-    counter_label(time_label)
+    counter_label(time_label, batt_label)
     add_hr['state']='disabled'
     add_min['state']='disabled'
     add_sec['state']='disabled'
@@ -63,14 +98,18 @@ def Stop():
     running = False
    
 # Reset function of the stopwatch
-def Reset(time_label):
+def Reset(time_label, batt_label, onelap_label):
     global counter
+    global one_lap
+    global batt
     counter = 0
+    one_lap = 0
+    batt = 0
     # If rest is pressed after pressing stop.
     Stop()
-    hrs, mins, secs = S2Time(counter)
-    display = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(hrs, mins, secs)
-    time_label['text'] = display
+    time_label['text'] = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(0, 0, 0)
+    batt_label['text'] = "Batt: %d" % (batt)
+    onelap_label['text'] = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(0, 0, 0)
    
 root = tk.Tk()
 root.title("Stopwatch")
@@ -81,86 +120,26 @@ time_label = tk.Label(root, text = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(0, 0, 
 time_label.pack()
 batt_label = tk.Label(root, text = "Batt: %d" % (batt), fg = "black", font = "Verdana 20")
 batt_label.pack()
+onelap_label = tk.Label(root, text = '{0:02.0f}:{1:02.0f}:{2:02.0f}'.format(0, 0, 0), fg = "black", font = "Verdana 10")
+onelap_label.pack()
 f = tk.Frame(root)
-add_hr = tk.Button(f, text='+1hr', width=6, command=lambda:add_time(time_label, 'hr'))
-add_min = tk.Button(f, text='+1min', width=6, command=lambda:add_time(time_label, 'min'))
-add_sec = tk.Button(f, text='+1sec', width=6, command=lambda:add_time(time_label, 'sec'))
-start = tk.Button(f, text='Start', width=6, command=lambda:Start(time_label))
+add_hr = tk.Button(f, text='+1hr', width=6, command=lambda:add_time(time_label, onelap_label, 'hr', 'counter'))
+add_min = tk.Button(f, text='+1min', width=6, command=lambda:add_time(time_label, onelap_label, 'min', 'counter'))
+add_sec = tk.Button(f, text='+1sec', width=6, command=lambda:add_time(time_label, onelap_label, 'sec', 'counter'))
+add_hr_onelap = tk.Button(f, text='+1hr', width=6, command=lambda:add_time(time_label, onelap_label, 'hr', 'onelap'))
+add_min_onelap = tk.Button(f, text='+1min', width=6, command=lambda:add_time(time_label, onelap_label, 'min', 'onelap'))
+add_sec_onelap = tk.Button(f, text='+1sec', width=6, command=lambda:add_time(time_label, onelap_label, 'sec', 'onelap'))
+start = tk.Button(f, text='Start', width=6, command=lambda:Start(time_label, batt_label))
 stop = tk.Button(f, text='Stop',width=6,state='disabled', command=Stop)
-reset = tk.Button(f, text='Reset',width=6, state='normal', command=lambda:Reset(time_label))
+reset = tk.Button(f, text='Reset',width=6, state='normal', command=lambda:Reset(time_label, batt_label, onelap_label))
 f.pack(anchor = 'center', pady = 5)
 add_hr.grid(row = 0, column = 0)
 add_min.grid(row = 0, column = 1)
 add_sec.grid(row = 0, column = 2)
-start.grid(row = 1, column = 0)
-stop.grid(row = 1, column = 1)
-reset.grid(row = 1, column = 2)
+add_hr_onelap.grid(row = 1, column = 0)
+add_min_onelap.grid(row = 1, column = 1)
+add_sec_onelap.grid(row = 1, column = 2)
+start.grid(row = 2, column = 0)
+stop.grid(row = 2, column = 1)
+reset.grid(row = 2, column = 2)
 root.mainloop()
-
-
-# #Import the required Libraries
-# from tkinter import *
-# from tkinter import ttk
-
-# #Create an instance of Tkinter frame
-# win= Tk()
-
-# #Set the geometry of Tkinter frame
-# win.geometry("750x250")
-
-# def display_text():
-#    global entry
-#    string= entry.get()
-#    label.configure(text=string)
-
-# #Initialize a Label to display the User Input
-# label=Label(win, text="", font=("Courier 22 bold"))
-# label.pack()
-
-# #Create an Entry widget to accept User Input
-# entry= Entry(win, width= 40)
-# entry.focus_set()
-# entry.pack()
-
-# #Create a Button to validate Entry Widget
-# ttk.Button(win, text= "Okay",width= 20, command= display_text).pack(pady=20)
-
-# win.mainloop()
-
-
-
-
-# import time
-# race_time = 2700
-# battery = 100
-
-# def countdown(total, one_lap):
-#     global race_time
-#     race_time = total * 60
-#     global battery
-#     remaining_time = total * 60
-    
-#     while remaining_time >= 0:
-#         mins, secs = divmod(remaining_time, 60)
-#         timer = '{:02d}:{:02d}'.format(mins, secs)
-#         battery = ((remaining_time + one_lap) / (race_time + one_lap)) * 100
-#         print('Time Left: ', timer, '     Battery Lower Limit: ', round(battery, 1), end="\r")
-#         time.sleep(1)
-#         remaining_time -= 1
-        
-#     remaining_time += 2
-    
-#     while True:
-#         mins, secs = divmod(remaining_time, 60)
-#         timer = '{:02d}:{:02d}'.format(mins, secs)
-#         battery = ((-remaining_time + one_lap) / (race_time + one_lap)) * 100
-#         print('Time Left: ', timer, '     Battery Lower Limit: ', round(battery, 1), end="\r")
-#         time.sleep(1)
-#         remaining_time += 1
-
-
-# total = input('Total Race Time (in minute): ')
-# one_lap = input('Time for one Lap (in second): ')
-# print('\n')
-
-# countdown(int(total), int(one_lap))
